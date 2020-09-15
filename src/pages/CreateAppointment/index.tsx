580503579f5161389d841e4e381120c58f066536
Samuel Pietra <samuelpietra@outlook.com>
2020-09-15 19:52:48 -0300

    wip

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -121,7 +121,7 @@ const CreateAppointment: React.FC = () => {
       date.setHours(selectedHour);
       date.setMinutes(0);
 
-      await api.post('appointments', {
+      await api.post('/appointments', {
         provider_id: selectedProvider,
         date,
       });
